0121168e7ee1911383c34f5a9127f1bd1b4021b4
zsecq94 <zsecq94@gmail.com>
2023-03-19 23:06:42 +0900

    123

--- 23/03/16_요세푸스 문제.py
+++ 23/03/16_요세푸스 문제.py
@@ -4,18 +4,21 @@ a, b = list(map(int, input().split()))
 
 lst = []
 for i in range(1, a+1):
-    lst.append(i)
+    lst.append([i, 0])
 
 result = []
-a = 0
-b = -1
-while len(lst) > 0:
-    if a == 3:
-        result.append(lst[b])
-        lst.remove(lst[b])
-        b -= 1
-        a = 0
-    if b > len(lst):
-        b = 0
+a = -1
+b = 0
+c = 0
+while a != len(lst):
     a += 1
-    b += 1
+    if a == 2:
+        if lst[a][1] == 0:
+            result.append(lst[a][0])
+            lst[a][1] = 1
+            c += 1
+
+            if a > len(lst):
+                a = -1
+        else:
+            a -= 1
