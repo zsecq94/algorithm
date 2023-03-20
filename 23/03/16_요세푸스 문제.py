afbfd05d041bc6593e71bd26c7347c70cda93720
zsecq94 <zsecq94@gmail.com>
2023-03-20 19:58:01 +0900

    요세푸스

--- 23/03/16_요세푸스 문제.py
+++ 23/03/16_요세푸스 문제.py
@@ -1,24 +1,29 @@
 import sys; sys.stdin = open('123.txt', 'r')
 
-a, b = list(map(int, input().split()))
+a, q = list(map(int, input().split()))
 
 lst = []
 for i in range(1, a+1):
     lst.append([i, 0])
-
 result = []
 a = -1
-b = 0
+b = -1
 c = 0
-while a != len(lst):
+while c != len(lst):
     a += 1
-    if a == 2:
-        if lst[a][1] == 0:
-            result.append(lst[a][0])
-            lst[a][1] = 1
+    b += 1
+    if b > len(lst)-1:
+        b = 0
+    if lst[b][1] == 0:
+        if a == q-1:
+            result.append(lst[b][0])
+            lst[b][1] = 1
             c += 1
-
+            a = -1
             if a > len(lst):
                 a = -1
-        else:
-            a -= 1
+    else:
+        a -= 1
+
+a = ", ".join(map(str, result))
+print(f'<{a}>')
